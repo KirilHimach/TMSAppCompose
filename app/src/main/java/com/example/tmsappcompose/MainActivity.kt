5666ccc1eaf50c167ce7d80b0190cb47374589bf
Kiril <himachkiril@gmail.com>
2023-05-01 12:07:27 +0300

    Task 6 Himach

--- app/src/main/java/com/example/tmsappcompose/MainActivity.kt
+++ app/src/main/java/com/example/tmsappcompose/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.tmsappcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tmsappcompose.ui.theme.TMSAppComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +28,115 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TMSAppComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    FirstScreen()
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun FirstScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Welcome to the app",
+            fontSize = 26.sp,
+            fontStyle = FontStyle.Italic
+        )
+        val username = inputUsername()
+        val password =  inputPassword()
+        Button(
+            onClick = { /*TODO*/ },
+            enabled = username.isNotEmpty() && password.length >= 8,
+            modifier = Modifier.padding(vertical = 12.dp)
+        ) {
+            Text(text = "Log in")
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    TMSAppComposeTheme {
-        Greeting("Android")
+fun inputUsername() : String {
+    var username by remember {
+        mutableStateOf("")
     }
+
+    OutlinedTextField(
+        value = username,
+        onValueChange = { username = it },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 34.dp),
+        label = { Text(text = "Username") },
+        placeholder = { Text(text = "Username or password") },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        leadingIcon = { Icon(imageVector = Icons.Filled.Email, null) },
+        trailingIcon = {
+            IconButton(onClick = { username = "" }) {
+                Icon(imageVector = Icons.Filled.Clear, null)
+            }
+        },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Green,
+            focusedLabelColor = Color.Blue,
+            unfocusedBorderColor = Color.Gray,
+            unfocusedLabelColor = Color.Gray,
+            leadingIconColor = Color.Black
+        )
+    )
+    return username
+}
+
+@Composable
+fun inputPassword() : String {
+    var password by remember {
+        mutableStateOf("")
+    }
+    var visibilityPassword by remember {
+        mutableStateOf(false)
+    }
+
+    OutlinedTextField(
+        value = password,
+        onValueChange = { password = it },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 34.dp),
+        label = { Text(text = "Password") },
+        placeholder = { Text(text = "Password") },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        leadingIcon = { Icon(imageVector = Icons.Filled.Lock, null) },
+        trailingIcon = {
+            val image = if (visibilityPassword)
+                Icons.Filled.Visibility
+            else Icons.Filled.VisibilityOff
+
+            IconButton(onClick = { visibilityPassword = !visibilityPassword }) {
+                Icon(imageVector = image, null)
+            }
+        },
+        visualTransformation = if (visibilityPassword)
+            VisualTransformation.None
+        else PasswordVisualTransformation(),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Green,
+            focusedLabelColor = Color.Cyan,
+            unfocusedBorderColor = Color.Gray,
+            unfocusedLabelColor = Color.Gray,
+            leadingIconColor = Color.Black
+        )
+    )
+    return password
 }
